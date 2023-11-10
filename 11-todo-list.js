d4452fb3545f45dca13c228b3d8ad2e9e341f1bb
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-11-10 15:16:37 +0530

    added to do name and due date

--- 11-todo-list.js
+++ 11-todo-list.js
@@ -5,8 +5,12 @@ renderTodoList();
         let todoListHTML = '';
 
         for (let i = 0 ; i < todoList.length ; i++){
-            const todo = todoList[i];
-            const html = `<p>${todo} <button 
+            const todoObject = todoList[i];
+            // const name = todoObject.name;
+            // const dueDate = todoObject.dueDate;
+            const { name, dueDate } = todoObject;
+            
+            const html = `<p>${name} ${dueDate}<button 
                 onclick = "
                     todoList.splice(${i}, 1);
                     renderTodoList();
@@ -21,8 +25,14 @@ renderTodoList();
 function addTodo (){
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
-    todoList.push(name);
-    console.log (todoList)
+
+    const inputDuedateElement = document.querySelector ('.js-due-date-input');
+    const dueDate = inputDuedateElement.value;
+    todoList.push({
+        name : name,              //    you can simply write    
+        dueDate : dueDate        //    {name,
+                                //     dueDate} because key and and values are equal
+    });
 
     // To remove the value from input box after clicking add button.
     inputElement.value = '';
