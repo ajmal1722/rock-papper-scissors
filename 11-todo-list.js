3ee236da7296a8f1a27bc4f659fe6e7d89647362
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-11-10 10:39:59 +0530

    creaed a delete button and made it work

--- 11-todo-list.js
+++ 11-todo-list.js
@@ -6,9 +6,14 @@ renderTodoList();
 
         for (let i = 0 ; i < todoList.length ; i++){
             const todo = todoList[i];
-            const html = `<p>${todo}</p>`;
+            const html = `<p>${todo} <button 
+                onclick = "
+                    todoList.splice(${i}, 1);
+                    renderTodoList();
+                ">Delete</button></p>`;
             todoListHTML += html;
         }
+        
     
         document.querySelector('.js-todo-list').innerHTML = todoListHTML;
     }
